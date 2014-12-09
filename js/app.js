51ccf43fbaa42eef07a9d4d96703ec0c14b11740
Jason Ardell <ardell@gmail.com>
2014-12-09 09:55:39 -0700

    Get basic phaser starter kit working with sinatra and puma.

---
+++ js/app.js
@@ -0,0 +1,14 @@
+// Put all your game code within this function definition
+window.onload = function() {
+  var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create });
+
+  function preload () {
+    game.load.image('logo', 'images/phaser.png');
+  }
+
+  function create () {
+    var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
+    logo.anchor.setTo(0.5, 0.5);
+  }
+};
+
